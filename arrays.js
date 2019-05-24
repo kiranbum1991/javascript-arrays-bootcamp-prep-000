023039db5d75d8db9b2c61485d8618877126173d
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-24 14:39:32 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var candyString = "foo";
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString,...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function 
