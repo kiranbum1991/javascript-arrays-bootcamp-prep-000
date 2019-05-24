e491f65248400ca1dad6a34e38e20c56cd84e4aa
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-24 14:54:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,5 +34,6 @@ function removeElementFromBeginningOfArray (chocolateBars) {
 }
 
 function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
-  chocolateBars.pop;
+  chocolateBars.pop();
+  return chocolateBars;
 }
